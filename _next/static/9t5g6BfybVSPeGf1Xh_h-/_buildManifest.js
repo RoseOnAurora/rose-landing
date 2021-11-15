025ceaf24ee3d46fa1cbdd9bf9fedc5b5652595e
Grandi <grandirose@protonmail.com>
2021-11-15 13:16:59 -0500

    Updates

--- _next/static/9t5g6BfybVSPeGf1Xh_h-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-6b6ec520e66102b18553.js","static/chunks/pages/index-78e9136d6f4a8b657fb0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
